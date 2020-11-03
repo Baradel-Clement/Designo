b98ff255bc4edb5e23c2ebee7352483addeadf72
Clément Baradel <baradelclement@gmail.com>
2020-11-03 18:12:09 +0100

    .

--- srcs/1-scripts/app.js
+++ srcs/1-scripts/app.js
@@ -30,3 +30,31 @@ designCards.forEach(card => {
         card.lastElementChild.firstElementChild.classList.add('peach')
     })
 })
+
+// Design cards end
+
+// Contact form 
+
+const inputs = document.querySelectorAll('input');
+const form = document.querySelector('#contact-form');
+const textareas = document.querySelectorAll('textarea');
+
+if (form) {
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        inputs.forEach(input => {
+            if (input.value == '' || input.value == null) {
+                input.nextElementSibling.classList.add('active');
+            } else {
+                input.nextElementSibling.classList.remove('active');
+            }
+        })
+        textareas.forEach(textarea => {
+            if (textarea.value == '' || textarea.value == null) {
+                textarea.nextElementSibling.classList.add('active');
+            } else {
+                textarea.nextElementSibling.classList.remove('active');
+            }
+        })
+    })
+}
